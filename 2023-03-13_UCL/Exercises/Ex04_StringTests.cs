b9890b017ab60de4f09e6ea6c0e56cc41a96053c
Michael Rulle <michael@rulle.xyz>
2023-03-14 17:18:15 +0100

    added files and solutions to most exercises in the 2023 folder

---
+++ 2023-03-13_UCL/Exercises/Ex04_StringTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Exercises
+{
+    public class Ex04_StringTests
+    {
+        [Fact]
+        public void A_default_person_has_a_non_empty_star_sign()
+        {
+            // Arrange
+            var person = new Person();
+
+            // Act
+            string starSign = person.GetStarSign();
+
+            // Assert
+            starSign.Should().NotBeNull().And.NotBeEmpty();
+            //throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void This_test_framework_ignoring_casing_is_xunit()
+        {
+            // Arrange
+            string expectedFrameworkName = "xunit";
+
+            // Act
+            string frameworkName = GetTestFramework();
+
+            // Assert
+            frameworkName.Should().BeEquivalentTo(expectedFrameworkName);
+            //throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void Donald_Duck_has_a_nephew_named_Louie()
+        {
+            // Arrange
+            string expectedNephew = "Louie";
+
+            // Act
+            string[] nephews = GetNephewsOfDonaldDuck();
+
+            // Assert
+            nephews.Should().NotBeNullOrEmpty().And.Contain(expectedNephew);
+            //throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void The_Danish_alphabeth_has_29_letters()
+        {
+            // Arrange
+            int expectedLength = 29;
+
+            // Act
+            string alphabet = GetDanishAlphabeth();
+
+            // Assert
+            alphabet.Should().NotBeNullOrEmpty().And.HaveLength(expectedLength);
+            //throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void The_error_message_matches_Foo_and_Bar_in_that_order()
+        {
+            // Act
+            string errorMessage = GetErrorMessage();
+
+            // Assert
+            errorMessage.Should().NotBeNullOrEmpty().And.ContainAll(new[] {"Foo", "Bar"});
+            //throw new NotImplementedException();
+        }
+
+        #region Helpers
+        private class Person
+        {
+            public string GetStarSign() => "Taurus";
+        }
+
+        public static string GetTestFramework() => "xUniT";
+
+        public static string[] GetNephewsOfDonaldDuck() => new[] { "Huey", "Louie", "Dewey" };
+
+        public static string GetDanishAlphabeth() => "ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
+
+        public static string GetErrorMessage() => "Yada Yada Foo Yada Yada Bar Yada Yada";
+        #endregion
+    }
+}
---
+++ 2023-03-13_UCL/Exercises/Ex06_DictionaryTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Exercises
+{
+    public class Ex06_DictionaryTests
+    {
+        [Fact]
+        public void Translating_1337_to_leet_speak_is_leet()
+        {
+            // Arrange
+            var expectedKey = 1337;
+            var expectedValue = "leet";
+
+            // Act
+            KeyValuePair<int, string> leetItem = new KeyValuePair<int, string>(expectedKey, expectedValue);
+            Dictionary<int, string> leetSpeak = GetLeetSpeak();
+
+            // Assert
+            leetSpeak.Should().NotBeNull().And.Contain(leetItem);
+            //throw new NotImplementedException();
+        }
+
+        #region Helpers
+        private static Dictionary<int, string> GetLeetSpeak() => new()
+        {
+            [1337] = "leet"
+        };
+        #endregion
+    }
+}
